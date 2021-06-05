a80f01be86101b64a174bdcf1e220a8b8d65d409
Jun <junchenglai1212@gmail.com>
2021-06-05 12:14:31 +0800

    change 2

--- password.py
+++ password.py
@@ -6,8 +6,8 @@
 # 如果不正確 就印出 "密碼錯誤！ 還有＿次機會！"
 
 password = 'a123456'
-i = 3
-while True:
+i = 3 #剩餘機會
+while i > 0:
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功！')
@@ -15,5 +15,3 @@ while True:
     else:
     	i = i - 1
     	print('密碼錯誤！ 還有',i,'機會！')
-    	if i == 0:
-    	    break
